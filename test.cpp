604a96da39784b675cc743971d64affbd286853b
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-03-30 17:48:28 -0400

    ammended make file to account for test copied gtests from mimir to test on own machine

--- test.cpp
+++ test.cpp
@@ -19,7 +19,49 @@ TEST(NSE, GET_RANK){
 
 
 }
+TEST(NSE, FIND_ENTRY){
 
+	NameSurferDataBase db("NamesData.txt");
+
+	NameSurferEntry temp;
+
+	temp = db.findEntry("Jennifer");
+
+	ASSERT_FALSE(temp.empty());
+
+
+}
+
+TEST(NSE, NOT_FOUND_ENTRY){
+	
+	NameSurferDataBase db("NamesData.txt");
+
+	NameSurferEntry temp;
+	temp = db.findEntry("None");
+
+	ASSERT_TRUE(temp.empty());
+}
+
+TEST(NSE, GET_RANK_ASHLEY){
+	NameSurferDataBase database("NamesData.txt");
+	
+	NameSurferEntry temp;
+	
+	temp = database.findEntry("Ashley");
+	
+	ASSERT_EQ(temp.getRank(1990),1);
+
+}
+TEST(NSE, get_rank_Bobbye){
+	NameSurferDataBase database("NamesData.txt");
+	
+	NameSurferEntry temp;
+	
+	temp = database.findEntry("Bobbye");
+	
+	ASSERT_EQ(temp.getRank(1930),687);
+
+}
 int main (int argc, char** argv){
 
 	::testing ::InitGoogleTest(&argc, argv);
